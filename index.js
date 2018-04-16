0a6b0f8a4f023239a897cbec02327d5a6bf87bd5
Jogiter <jogiter.g@gmail.com>
2018-04-16 16:59:33 +0800

    update

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
 const loaderUtils = require("loader-utils");
-const assign = require("object-assign");
 const SSI = require("./lib/ssi");
 
-function getLoaderConfig(context) {
-	const query = loaderUtils.parseQuery(context.query);
-	const configKey = query.config || 'ssiLoader';
-	const config = context.options &&
-              context.options.hasOwnProperty(configKey) ?
-              context.options[configKey] :
-              {};
-
-	delete query.config;
-
-	return assign(query, config);
-}
-
 module.exports = function (source) {
-  const config = getLoaderConfig(this);
-	const ssi = new SSI(config);
+  const options = loaderUtils.getOptions(this);
+	const ssi = new SSI(options);
 
 	this.cacheable && this.cacheable();
 
